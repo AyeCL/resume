3b5397eab03b291e6ded60605785283b57a15d34
Aayush Lamichhane <ayeyell@colostate.edu>
2022-04-15 12:20:23 -0600

    add react-device-detect dependency and add padding on header.js for mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { MobileView } from 'react-device-detect'
 
 function Header() {
   return (
     <div className='container text-center topic' style={{ paddingTop: '1vh', paddingBottom: '2vh' }}>
+      <MobileView>
+        <div style={{ paddingTop: '1em' }}></div>
+      </MobileView>
       <h1>
         Aayush Lamichhane
       </h1>
