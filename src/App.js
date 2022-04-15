ce8bd69595b84648911723aa17a4d04af367b2fe
Aayush Lamichhane <ayeyell@colostate.edu>
2022-04-14 23:57:40 -0600

    start resume officially

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
-import NavigationMenu from './NavigationMenu';
+import { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 function App() {
+
   return (
-    <div>
-      <NavigationMenu />
+    <div className='container'>
+      <h1 className='text-center' style={{ paddingTop: '1vh' }}>
+        Aayush Lamichhane
+      </h1>
+      <div className='text-center'>
+          aayush@lamichhane.us | (720) 935-1478
+      </div>
     </div>
   );
 }
