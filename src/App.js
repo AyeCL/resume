843f27e95a462bdb78115585c2d100bca28588e7
Aayush Lamichhane <ayeyell@colostate.edu>
2022-04-15 01:47:37 -0600

    lamichaney

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function Header() {
   return (
     <div className='container text-center topic' style={{ paddingTop: '1vh', paddingBottom: '2vh' }}>
       <h1>
-        Aayush Lamichhane
+        Aayush Lamichhaney
       </h1>
       <div>
         <FontAwesomeIcon icon={faEnvelope} /> aayush@lamichhane.us &nbsp;|&nbsp; <FontAwesomeIcon icon={faPhone} /> (720) 935-1478
