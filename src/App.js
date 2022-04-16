4106bffa057177e64f6e33fe1b03f99817cd51de
Aayush Lamichhane <ayeyell@colostate.edu>
2022-04-15 19:04:29 -0600

    added footer component for both mobile and browser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import Header from './components/Header';
 import Education from './components/Education';
 import Technologies from './components/Technologies';
@@ -7,7 +10,9 @@ import WorkExperience from './components/WorkExperience';
 import Projects from './components/Projects';
 import RelevantCoursework from './components/RelevantCoursework';
 import LeadershipExperience from './components/LeadershipExperience';
+import Footer from './components/Footer';
 
+library.add(fas, fab);
 
 function App() {
   return (
@@ -19,6 +24,7 @@ function App() {
       <Projects />
       <RelevantCoursework />
       <LeadershipExperience />
+      <Footer />
     </>
   );
 }
