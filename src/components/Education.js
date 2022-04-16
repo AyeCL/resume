fb52d8b61dbfda16edcef2f1c24dbd555b23529a
Aayush Lamichhane <ayeyell@colostate.edu>
2022-04-16 04:55:23 -0600

    remove unused import

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Lightbulb, LightbulbOff } from 'react-bootstrap-icons';
 
 function Education(props) {
