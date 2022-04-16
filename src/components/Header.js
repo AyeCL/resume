ee3927e54b73a464ef3d754e81def0dd6e4bba94
Aayush Lamichhane <ayeyell@colostate.edu>
2022-04-16 05:07:50 -0600

    idek at this point its too late at night

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { MobileView } from 'react-device-detect'
-import { Row, Col } from 'react-bootstrap'
 
-function Header() {
+function Header(props) {
   return (
     <div className='container text-center topic' style={{ paddingTop: '1vh', paddingBottom: '2vh' }}>
       <MobileView>
         <div style={{ paddingTop: '1vh' }}></div>
       </MobileView>
-      <Row>
-        <Col>
-          <h1>
-            Aayush Lamichhane
-          </h1>
-        </Col>
-      </Row>
+      <h1>
+        Aayush Lamichhane
+      </h1>
       <div>
         <FontAwesomeIcon icon="fa-solid fa-envelope" /> aayush@lamichhane.us &nbsp;|&nbsp; <FontAwesomeIcon icon="fa-solid fa-phone" /> (720) 935-1478
       </div>
