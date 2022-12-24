9c021320dc08e3645e02f8521fbc0c0cc8efca06
Aayush Lamichhane <ayeyell@colostate.edu>
2022-12-24 16:41:46 -0700

    bug fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Education from './components/Education';
 import Technologies from './components/Technologies';
 import WorkExperience from './components/WorkExperience';
 import Projects from './components/Projects';
-import RelevantCoursework from './components/RelevantCoursework';
+// import RelevantCoursework from './components/RelevantCoursework';
 import LeadershipExperience from './components/LeadershipExperience';
 import Footer from './components/Footer';
 import useLocalStorage from 'use-local-storage'
